a9fb26ca837dc4d86e52ac2f7309f40e8549563e
User <maspagkaliwangan@tip.edu.ph>
2026-01-29 12:55:11 +0800

    Add login feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,3 +4,11 @@ function App() {
     const users = ['Alice','Bob','Charlie'];
     return <UserList users={users} />;
 }
+
+const isLoggedIn = true;
+
+return (
+    <div>
+        {isLoggedIn ? <p>Welcome back!</p> : <p>Please log in</p>}
+    </div>
+);
